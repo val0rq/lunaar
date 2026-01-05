5292253dd8cd9b85eaaca376c43e5378ba1a239b
val0rq <csgo.real.god@gmail.com>
2026-01-05 20:28:46 +0200

    Add files via upload

---
+++ src/main/java/com/user/lunarlite/LiteClient.java
@@ -0,0 +1,10 @@
+package com.user.lunarlite;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LiteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // TODO: Add HUD rendering
+    }
+}
